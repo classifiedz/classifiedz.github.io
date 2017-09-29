f7171a3ad8df86fce6be1b924ccc44a9089028bf
laurie-y <laurie.yung@outlook.com>
2017-09-29 13:01:52 -0400

    Added contact preference for posting ad form #9

--- resources/views/postAd.blade.php
+++ resources/views/postAd.blade.php
@@ -6,7 +6,7 @@
   <div class="row-fluid">
     <div class="container col-sm-8">
       <h2>Post an Ad</h2>
-      <form action="{{ route('register') }}">
+      <form action="{{ route('postAd') }}">
         <div class="form-group">
           <label for="sel1">What category does your product fall under:</label>
           <select class="form-control" id="category">
@@ -28,9 +28,21 @@
           <textarea class="form-control" rows="5" id="description" placeholder="Describe what you're selling... "></textarea>
         </div>
         <div class="form-group">
-        <label for="image">Upload an image of your product:</label>
-        </br>
-        <button type="button" class="btn btn-primary">Upload</button>
+          <label for="image">Upload an image of your product:</label>
+          </br>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+        <div class="form-group">
+          <label for="contact">How would you prefer to be contacted?</label>
+          <div class="radio">
+            <label class="px-4"><input type="radio" name="contact" value="call"> Call</label>
+          </div>
+          <div class="radio">
+            <label class="px-4"><input type="radio" name="contact" value="text"> Text</label>
+          </div>
+          <div class="radio">
+            <label class="px-4"><input type="radio" name="contact" value="email"> Email</label>
+          </div>
         </div>
       </form>
     </div>
