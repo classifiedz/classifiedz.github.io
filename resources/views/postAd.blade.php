b597b838e86ece52948a1e812b023087eb873a70
laurie-y <laurie.yung@outlook.com>
2017-09-29 12:42:54 -0400

    Created basic template for Post Ad page #9

---
+++ resources/views/postAd.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid py-5">
+  <div class="row-fluid">
+    <div class="container col-sm-8">
+      <h2>Post an Ad</h2>
+
+    </div>
+  </div>
+</div>
+
+@endsection
