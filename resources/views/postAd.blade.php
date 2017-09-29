dff066e8a9da0eeb7ed8cf84261222323580a308
laurie-y <laurie.yung@outlook.com>
2017-09-29 12:47:45 -0400

    Added category specification for posting ad #9

--- resources/views/postAd.blade.php
+++ resources/views/postAd.blade.php
@@ -6,6 +6,20 @@
   <div class="row-fluid">
     <div class="container col-sm-8">
       <h2>Post an Ad</h2>
+      <form action="{{ route('register') }}">
+        <div class="form-group">
+          <label for="sel1">What category does your product fall under:</label>
+          <select class="form-control" id="sel1">
+            <option>---</option>
+            <option>Vehicles</option>
+            <option>Pets</option>
+            <option>Fashion</option>
+            <option>Home Appliances</option>
+            <option>Electronics</option>
+            <option>Entertainment</option>
+          </select>
+        </div>
+      </form>
 
     </div>
   </div>
