908fb16055d8cd0f7490272e9df804452f923373
Kevin Camellini <kevincamellini@gmail.com>
2017-10-02 15:31:37 -0400

    Another Travis-CI failed build Fix Attempt 8, Issue #62

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,6 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    use Illuminate\Support\Facades\Schema; 
     public function boot()
     {
         //
