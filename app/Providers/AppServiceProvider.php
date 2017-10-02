51557ba56fc04af9fbb42b37f88b21735a176cfa
Kevin Camellini <kevincamellini@gmail.com>
2017-10-02 15:20:54 -0400

    Another Travis-CI failed build Fix Attempt 6, Issue #62

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    use Illuminate\Support\Facades\Schema;
+    
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 
     /**
