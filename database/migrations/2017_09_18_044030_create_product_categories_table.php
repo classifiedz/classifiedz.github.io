3ce0c68ee072266831cd1e002412d1b878603acd
monnyy <gnekouy@gmail.com>
2017-10-03 15:13:40 -0400

    Update 2017_09_18_044030_create_product_categories_table.php

--- database/migrations/2017_09_18_044030_create_product_categories_table.php
+++ database/migrations/2017_09_18_044030_create_product_categories_table.php
@@ -17,7 +17,6 @@ class CreateProductCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->timestamps();
-            $table->string('pc_id')->unique();
         });
     }
 
