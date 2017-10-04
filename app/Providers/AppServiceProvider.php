52db6fa2cf3610b0166768ae4c037f54d57f048b
Brandon Balala <brandon.yvan1229@gmail.com>
2017-10-04 15:41:39 -0400

    Able to access all categories in master layout #101

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema; 
+use Illuminate\Support\Facades\Schema;
+use App\ProductCategory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Schema::defaultStringLength(191);
+
+        view()->composer('layouts.app', function($view) {
+            $categories = ProductCategory::where('parent_category_id', null)->get();
+
+            $view->with('categories', $categories);
+        });
     }
 
     /**
