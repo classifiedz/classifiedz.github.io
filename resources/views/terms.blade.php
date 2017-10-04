e55bd07b0b433994e1f9bfaa210a3896b91ce5a9
rami186 <ryahia94@gmail.com>
2017-10-04 00:05:38 -0400

    Terms of service page added issue #96

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1 class="display-1">Terms of Service</h1>
+
+<p class="h2">1. General</p>
+
+<p>Welcome to Classifiedz. Thanks for stopping by. These Terms of Use, the Privacy Policy and all policies posted on our site set out the terms on which we offer you access to and use of our site, services, applications and tools (collectively "Services"). You can find an overview of our policies here. All policies, the Mobile Devices Terms, and the Privacy Policy are incorporated into these Terms of Use. You agree to comply with all the above when accessing or using our Services. 
+
+Classifiedz is provided to you by SOEN 341 students, 1455 boulevard de Maisonneuve ouest
+
+In order to access and use some or a portion of the Services, you may be required to register with us and set up an account with your email address and a password (your “Account”). The email address you provide will be your email address, and you are solely responsible for maintaining the confidentiality of your password. You are solely responsible for all activities that occur under your Account. Therefore, you should protect your password and make your password difficult for others to guess.
+
+You may connect to the Services with a third-party service and you give us permission to access, store, and use your information from that service as permitted by that service and as may be described in our Privacy Policy. If you believe your Account may have been compromised or misused, contact us immediately at Kijiji Customer Support.</p>
+ 
+@endsection
