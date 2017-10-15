fa045dd2a7165e5282389d7f982deff1855f4b35
Neqqash Hussin <neqqash96@gmail.com>
2017-10-15 06:00:30 -0400

    Email, Password etc.. will now be contained within the textbox to reduce space

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,10 +13,10 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" placeholder= "Email Address" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -70,7 +70,8 @@
             </div>
             </div>
         </div>
- <div class="col-sm-1 middle-border"></div>
+
+         <div class="col-sm-1 middle-border"></div>
                         <div class="col-sm-1"></div>
 
 
