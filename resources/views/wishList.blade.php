e2817d9550f5bfb00c987b8065ed6b9dc22e2a70
Swallow666 <liyuan0314@outlook.com>
2017-10-18 00:29:33 -0400

    #157 frontend enhancement of wishlist page
    This commit make the wishlist looks better.

--- resources/views/wishList.blade.php
+++ resources/views/wishList.blade.php
@@ -5,7 +5,7 @@
 <div class="row px-5 pt-4">
    
 	
-		<table class="table table-user-information">
+		<table class="table table-striped">
 			<tbody>
 				<h3>{{ Auth::user()->name }}'s Wish List</h3>
 				<tr>
@@ -13,17 +13,17 @@
 					<td><b>Details</b></td>
 					<td><b>Actions</b></td>
 				</tr>
+			   
 				@foreach(Auth::user()->wishlist as $item)
 					<tr>
 						<td>{{ $item->product->category->name }}</td>
-						<td>{{ $item->product->title }}</td>
+						<td><a href="/ads/{{ $item->product->id }}">{{ $item->product->title }}</a></td>
 						<td>
-							<span><a class="btn btn-secondary" href="/ads/{{ $item->product->id }}" role="button">See Ad</a></span>
 							<span>
 								<form  action="/wishlist/{{ $item->id }}" method="POST">
 					                {{ csrf_field() }}
 					                <input type="hidden" name="_method" value="DELETE">
-					                <button id="deleteWishlistBtn" type="submit" class="btn btn-danger">Delete</button>
+					                <button id="deleteWishlistBtn" type="submit" class="btn btn-outline-danger">Delete</button>
 				                </form>
 			            	</span>
 						</td>
