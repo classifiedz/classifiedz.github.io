a01db553f231a04f75ea177cb1b0e70d5b69757f
laurie-y <laurie.yung@outlook.com>
2017-10-18 22:07:19 -0400

    For User Story #83 Placeholder for contacting the seller was added and adjusted the alignment of the submission date

--- resources/views/viewAd.blade.php
+++ resources/views/viewAd.blade.php
@@ -5,8 +5,8 @@
 <div class="container py-5">
     <div class="row">
         <div class=" col-sm-6">
-            <img class="img-fluid" src="{{ asset($product->image) }}" alt="Post Image">
-            <small>This ad was posted on {{ date('F jS', strtotime($product->created_at)) }} </small>
+            <img class="img-fluid" width="100%" src="{{ asset($product->image) }}" alt="Post Image">
+            <p><small>This ad was posted on {{ date('F jS', strtotime($product->created_at)) }} </small></p>
         </div>
         <div class=" col-sm-6">
             <h2>{{ $product->title }}</h2>
@@ -16,6 +16,8 @@
           </br>
             <h5>Description: </h5>
             <p>{{ $product->description}}</p>
+            <p><small>Interested? Contact them <a href="#">now</a>!</small></p>
+
         </div>
     </div>
 </div>
