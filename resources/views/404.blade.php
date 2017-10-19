f628cb9cfabdf98087436bb6d4a420aea7c157ee
rami186 <ryahia94@gmail.com>
2017-10-19 03:50:09 -0400

    404 page set up #97
    still missing to style it and to adjust the routes

---
+++ resources/views/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="container">
+<!-- PAGE CONTENT CONTAINER -->
+<h3>404, website not found</h3>
+</div>
+@endsection
+<!-- End Page Cointainer -->
