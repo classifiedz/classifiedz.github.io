2101e93deb475a61b3ffb302c1e5eb23c941c894
Swallow666 <liyuan0314@outlook.com>
2017-11-02 17:24:23 -0400

    This commit modify your store page layout and add contact information of users on your store page #161

--- resources/views/yourstore.blade.php
+++ resources/views/yourstore.blade.php
@@ -2,16 +2,34 @@
 
 @section('content')
 
-<div class="row px-5 pt-4 justify-content-center">
+<div class="row-fluid pt-3">
+	<!-- BREADCRUMB -->
+	<ol class="breadcrumb">
+		<span class="h3">&nbsp;{{ Auth::user()->username }}'s Store<span class="badge badge-default">0 items</span></span>
+	</ol>
+	<!-- items should be count of product-->
+	<!-- END BREADCRUMB -->
+</div>
+
+<!--contact info here-->
+
+	<table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">	<span class="badge badge-primary">Contact</span></th>
+      <td>{{ Auth::user()->email }}</td>
+      <td>{{ Auth::user()->phoneNumber }}</td>
+    </tr>
+</tbody>
+</table>
 
 	
 
+<!-- ads here-->
+<div class="row px-5 pt-4>
+
 	<form method="POST"  enctype="multipart/form-data" action="{{ route('updateProfile') }}">
-	<tr>
-	<img class="img-thumbnail" alt="User Picture" src="https://www.safeocs.gov/cos/resources/app/images/av1_1.png"/ width="70px">
-	</tr>
-	<ul>{{ old('name') == null ? Auth::user()->name : old('name') }}</ul>
-	<td>Ad Posted</td>	
+	<td><h4>Ad Posted</h4></td>	
 	</form>
 	<table class="table table-user-information">
 			<tbody>
