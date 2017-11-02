78f9c84f2d5be3dca2371a6da6f7c7ad0a0d0dd1
HanGao2333 <rebeccahangao@gmail.com>
2017-11-02 16:44:19 -0400

    modified your store page
    Add user name, pic, ad area to allow user check basic information #161

--- resources/views/yourstore.blade.php
+++ resources/views/yourstore.blade.php
@@ -2,3 +2,36 @@
 
 @section('content')
 
+<div class="row px-5 pt-4 justify-content-center">
+
+	
+
+	<form method="POST"  enctype="multipart/form-data" action="{{ route('updateProfile') }}">
+	<tr>
+	<img class="img-thumbnail" alt="User Picture" src="https://www.safeocs.gov/cos/resources/app/images/av1_1.png"/ width="70px">
+	</tr>
+	<ul>{{ old('name') == null ? Auth::user()->name : old('name') }}</ul>
+	<td>Ad Posted</td>	
+	</form>
+	<table class="table table-user-information">
+			<tbody>
+					{{ csrf_field() }}
+					<tr>
+						<td>Add the ad here</td>
+						
+					</tr>
+					
+			</tbody>
+		</table>
+	@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+</div>
+@endsection
