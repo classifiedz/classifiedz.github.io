b1c7b73b9c0f0c3b4b794bb2d538346260f408d1
Brandon Balala <brandon.yvan1229@gmail.com>
2017-11-02 23:28:44 -0400

    Added HTTP tests to see that it redirects when you need to be authenticated + added wishlist db unit tests

--- tests/Feature/RouteResponseTest.php
+++ tests/Feature/RouteResponseTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class RouteResponseTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Testing homepage.
      *
      * @return void
      */
@@ -19,13 +19,114 @@ class RouteResponseTest extends TestCase
         $response->assertStatus(200);
     }
 
-     /**
-     * A basic test example.
+    /**
+     * Test privacy page.
      *
      * @return void
      */
-    public function testNonExistentPage()
+    public function testPrivacyPage()
     {
+        $response = $this->get('/privacy');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test posting policy page.
+     *
+     * @return void
+     */
+    public function testPostingPolicyPage()
+    {
+        $response = $this->get('/postingpolicy');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test refund page.
+     *
+     * @return void
+     */
+    public function testRefundPage()
+    {
+        $response = $this->get('/refund');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test terms page.
+     *
+     * @return void
+     */
+    public function testTermsPage()
+    {
+        $response = $this->get('/terms');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test wishlist page, making sure that a redirect occurs because you need 
+     * to be authenticated to access this page.
+     *
+     * @return void
+     */
+    public function testWishlistRedirectAuthentication()
+    {
+        $response = $this->get('/wishlist');
+
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Test create ad page, making sure that a redirect occurs because you need 
+     * to be authenticated to access this page.
+     *
+     * @return void
+     */
+    public function testCreateAdRedirectAuthentication()
+    {
+        $response = $this->get('/ad/create');
+
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Test profile page, making sure that a redirect occurs because you need 
+     * to be authenticated to access this page.
+     *
+     * @return void
+     */
+    public function testProfilePageRedirectAuthentication()
+    {
+        $response = $this->get('/profile');
+
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Test edit profile page, making sure that a redirect occurs because you need 
+     * to be authenticated to access this page.
+     *
+     * @return void
+     */
+    public function testEditProfileRedirectAuthentication()
+    {
+        $response = $this->get('/profile/edit');
+
+        $response->assertStatus(302);
+    }
+
+
+     /**
+     * Testing non existent page.
+     *
+     * @return void
+     */
+     public function testNonExistentPage()
+     {
         $response = $this->get('/THIS/ROUTE/DOES/NOT/EXIST');
 
         $response->assertStatus(404);
