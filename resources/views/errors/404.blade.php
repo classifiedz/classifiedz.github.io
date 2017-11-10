45773603f6478209833b22fc57232cb173368a75
rami186 <ryahia94@gmail.com>
2017-11-10 12:32:17 -0500

    #97 Custom 404 page set up with basic styling
    404 page with basic styling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>404 page</title>
+</head>
+<body style="background-color:#0275D8">
+<div style="height: 100%;width: 100%;display: flex;position: fixed;align-items: center;justify-content: center;font-size:150px;color:#FFFFFF;font-family:arial black;">404</div>
+</body>
+</html>
