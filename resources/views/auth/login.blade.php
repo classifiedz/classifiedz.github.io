d0d9525b64e84d5e7e6cfcd005c2edd3daf3d08f
Neqqash Hussin <neqqash96@gmail.com>
2017-11-11 19:25:29 -0500

    This will put the required information within the textbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,10 +20,10 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="sr-only">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" placeholder = "E-mail Address...."class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
