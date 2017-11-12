21fd983be365c526e9b96c696b7fa07b77865154
Neqqash Hussin <neqqash96@gmail.com>
2017-11-11 19:32:49 -0500

    lets make the login button as long as the other textboxes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-12 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
